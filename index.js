b6bc783ad705bd58800b5e56218faadd193e3cc6
juancinfante <juaninfantejj@gmail.com>
2024-09-02 10:25:50 -0300

    cors fixed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const {dbConnection} = require('./database/config');
@@ -8,18 +7,23 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 mongoose.set('strictQuery', false);
 
+const app = express();
+app.use(cors());
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+app.use(express.json());
+
+dbConnection();
+
 const PORT = 4001 || process.env.PORT;
+
 app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto: " + PORT);
 })
 
-app.use(express.json());
-app.use(cors());
 
-dbConnection();
 
 app.use('/api', require('./routes/usuario'));
 app.use('/api', require('./routes/album'));
