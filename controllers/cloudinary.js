b8bd6845c38389c9932df51475455e62f99e8419
juancinfante <juaninfantejj@gmail.com>
2025-02-12 02:03:27 -0300

    bugs

--- controllers/cloudinary.js
+++ controllers/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY_CLOUDY,
-    api_secret: process.env.API_SECRET_CLOUDY,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const multer = require('multer');
